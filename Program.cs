d0d7a8bee6f7b739b7e8eb70313aa2c62eec7041
Gleidson Queiroz <gleidsonnq@gmail.com>
2022-11-23 04:03:31 -0300

    convertendoWiegand

--- Program.cs
+++ Program.cs
@@ -6,90 +6,43 @@ using System.Text;
 
 //bool is_usb0 = false;
 //string usb_serial = "";
-string DebugHex="";
-SerialPort usb1 = new SerialPort(@"/dev/ttyUSB1");
+SerialPort usb1 = new SerialPort(@"/dev/ttyUSB0");
 usb1.Encoding = Encoding.UTF8;
 usb1.BaudRate = 9600;
 usb1.Handshake = Handshake.None;
 usb1.ReadTimeout = 1;
 usb1.Open();
-SerialPort usb0 = new SerialPort(@"/dev/ttyUSB0");
-usb0.Encoding = Encoding.UTF8;
-usb0.BaudRate = 9600;
-usb0.Handshake = Handshake.None;
-usb0.ReadTimeout = 1;
-usb0.Open();
-SerialPort uart = new SerialPort(@"/dev/ttyS0");
-uart.Encoding = Encoding.UTF8;
-uart.BaudRate = 9600;
-uart.Handshake = Handshake.None;
-uart.ReadTimeout = 1;
-uart.Open();
-
-
 
 while(true){
     
-    /*
     try{
-        if(is_usb0){
-            is_usb0 = false;
-            SerialPort sp = new SerialPort(@"/dev/ttyUSB1");
-            sp.Encoding = Encoding.UTF8;
-            sp.BaudRate = 9600;
-            sp.Handshake = Handshake.None;
-            sp.ReadTimeout = 1000;
-            sp.WriteTimeout = 1000;
-            sp.Open();
-        }
-        else{
-            is_usb0 = true;
-            SerialPort sp = new SerialPort(@"/dev/ttyUSB0");
-            sp.Encoding = Encoding.UTF8;
-            sp.BaudRate = 9600;
-            sp.Handshake = Handshake.None;
-            sp.ReadTimeout = 1000;
-            sp.WriteTimeout = 1000;
-            sp.Open();
+        
+        int usb_read_byte = usb1.ReadByte();
+        int[] DebugW = new int[10];
+        if(usb_read_byte == 0x02){
+            for(int counter=0;counter<10;counter++){
+                usb_read_byte = usb1.ReadByte();
+                if(usb_read_byte<=57){
+                    DebugW[counter] = usb_read_byte - 48;
+                }
+                else{
+                    DebugW[counter] = usb_read_byte - 55;
+                }
+                
+            }
+            
+
+            int facility = DebugW[4]*16 + DebugW[5];
+            
+            int cardCode = DebugW[6]*16*16*16 + DebugW[7]*16*16 + DebugW[8]*16 + DebugW[9];
+            
+            string dadosTagSaida = facility.ToString() + cardCode.ToString();
+            Console.WriteLine(dadosTagSaida);
+            
         }
     }
     catch(TimeoutException){
-
-    }
-    */
-
-    try{
-        int valor = usb0.ReadByte();
-        int valor1 = usb1.ReadByte();
-        int valor2 = uart.ReadByte();
-        if(valor == '\x02'){
-            for(int counter=0;counter<12;counter++){
-                valor = usb0.ReadByte();
-                DebugHex = DebugHex + valor.ToString() + " ";
-            }
-        }
-        if(valor1 == '\x02'){
-            for(int counter=0;counter<12;counter++){
-                valor1 = usb1.ReadByte();
-                DebugHex = DebugHex + valor1.ToString() + " ";
-            }
-        }
-        if(valor2 == '\x02'){
-            for(int counter=0;counter<12;counter++){
-                valor2 = uart.ReadByte();
-                DebugHex = DebugHex + valor2.ToString() + " ";
-            }
-        }
-    }
-    catch(TimeoutException){
-
-    }
-
-
-
-    // See https://aka.ms/new-console-template for more information
-    Console.WriteLine(DebugHex);
-    
+    } 
 
 }
     
